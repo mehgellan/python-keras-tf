ff2bee7518deba16dbaba816b3f6948b9c185ef8
Meg Bauman <mbauman@google.com>
2019-12-17 15:02:38 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python with TF, Keras
+Using the tutorial here: https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
---
+++ keras_first_network.py
@@ -0,0 +1,9 @@
+# first neural network with keras tutorial
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+# load the dataset
+dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
+# split into input (X) and output (y) variables
+X = dataset[:,0:8]
+y = dataset[:,8]
